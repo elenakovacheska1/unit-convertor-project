3d09bc80403555e69089053d920f9882681f1d9f
Elena Kovacheska <kovacheskaelena@gmail.com>
2022-10-10 17:46:15 +0200

    click btn on enter

--- index.js
+++ index.js
@@ -35,3 +35,14 @@ function convertMass(value) {
     3
   )} pounds | ${value} pounds = ${(value / 2.204).toFixed(3)} kilos`;
 }
+
+// Execute a function when the user presses a key on the keyboard
+numbInputEl.addEventListener("keypress", function (event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    convertBtn.click();
+  }
+});
